2840734dbefeb4d9322423a14a89697d10ae3e30
Svetlana Proskurina <amourka@yandex.ru>
2022-07-17 00:38:30 +0300

    homework2 change comments

--- task1/Program.cs
+++ task1/Program.cs
@@ -4,10 +4,10 @@ int input  = Convert.ToInt32(Console.ReadLine());
 
 string number = input.ToString();
 //Console.WriteLine(number);
-int [] array = new int[number.Length]; // создаем новый массив длинной равной количеству символов в строке
+int [] array = new int[number.Length]; // создаем новый массив c элементами типа INT и длинной равной количеству символов в строке
 for (int i=0; i<number.Length; i++)
 {
-    array[i] = int.Parse(number[i].ToString()); //Преобразует строковое представление числа в эквивалентное ему 32-битовое целое число со знаком
+    array[i] = int.Parse(number[i].ToString()); //Преобразует строковое представление числа в эквивалентное ему целое число 
 // Console.WriteLine(array[i]);
 }
 Console.WriteLine(array[1]);
